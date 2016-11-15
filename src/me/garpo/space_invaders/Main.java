07b38f1216861d40b91810eaa59e62a4912f35a6
Dolgan12 <dolgan12@gmail.com>
2016-11-15 06:26:57 +0100

    Added scafolding to Main

--- src/me/garpo/space_invaders/Main.java
+++ src/me/garpo/space_invaders/Main.java
@@ -1,7 +1,32 @@
 package me.garpo.space_invaders;
 
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
 /**
  * Created by dolgan12 on 2016-11-14.
  */
-public class Main {
+public class Main extends Application{
+    private Parent createContent() {
+        Pane root = new Pane();
+
+        return root;
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Scene scene = new Scene(createContent());
+        primaryStage.setTitle("Space Invaders");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+
 }
